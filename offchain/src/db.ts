b27ed6ecdd9abbcbfd4f3f686e72f0b39ade38c6
Peter <kompaszbence@gmail.com>
2025-08-01 14:02:37 +0300

    feat: new tables

--- offchain/src/db.ts
+++ offchain/src/db.ts
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { Swap, SwapRequest, SwapStatus } from "./types";
 
 const client = new Client({
   user: "user",
@@ -13,20 +14,28 @@ async function setup() {
     await client.connect();
     console.log("Connected to PostgreSQL");
 
-    // Example: Create a table
     await client.query(`
-      CREATE TABLE IF NOT EXISTS users (
+       CREATE TABLE IF NOT EXISTS swaps (
         id SERIAL PRIMARY KEY,
-        name TEXT NOT NULL
+        status TEXT NOT NULL CHECK (status IN ('created', 'in-progress', 'invalid', 'canceled', 'completed')),
+        type TEXT NOT NULL CHECK (type IN ('cosmos-evm', 'evm-cosmos')),
+        cosmosChainId NUMERIC NOT NULL,
+        evmChainId NUMERIC NOT NULL,
+        to TEXT NOT NULL,
+        sourceCoin TEXT NOT NULL,
+        targetCoin TEXT NOT NULL,
+        amount NUMERIC NOT NULL
       );
     `);
 
-    // Example: Insert a row
-    await client.query("INSERT INTO users (name) VALUES ($1)", ["Alice"]);
-
-    // Example: Query the table
-    const res = await client.query("SELECT * FROM users");
-    console.log("Users:", res.rows);
+    await client.query(`
+       CREATE TABLE IF NOT EXISTS auctions (
+        id SERIAL PRIMARY KEY,
+        status TEXT NOT NULL CHECK (status IN ('inactive', 'active')),
+        fillAmount NUMERIC NOT NULL,
+        fills NUMERIC DEFAULT 1
+      );
+    `);
   } catch (err) {
     console.error("Error:", err);
   } finally {
@@ -35,4 +44,50 @@ async function setup() {
   }
 }
 
-export { setup };
+async function createSwap(swap: Swap) {
+  const result = await client.query(
+    "INSERT INTO swaps (status, type, cosmosChainId, evmChainId, from, to, sourceCoin, targetCoin, amount) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+    [
+      swap.status,
+      swap.type,
+      swap.cosmosChainId,
+      swap.evmChainId,
+      swap.from,
+      swap.to,
+      swap.sourceCoin,
+      swap.targetCoin,
+      swap.amount,
+    ]
+  );
+
+  return result.rows[0].id;
+}
+
+async function updateSwapStatus(
+  swapId: string,
+  newSwapStatus: SwapStatus
+): Promise<Swap> {
+  const resp = await client.query("SELECT * FROM swaps WHERE id = $1");
+
+  if (!resp.rows.length) {
+    throw new Error(`No swap w/ id: ${swapId}`);
+  }
+
+  const result = await client.query(
+    "UPDATE swaps SET status = $1 WHERE id = $2",
+    [newSwapStatus, swapId]
+  );
+
+  return result.rows[0] as Swap;
+}
+
+async function createAuction(swapRequest: SwapRequest) {
+  const result = await client.query(
+    "INSERT INTO auctions (status, fillAmount, fills) VALUES ($1, $2, $3)",
+    ["live", swapRequest, 1]
+  );
+
+  return result.rows[0].id;
+}
+
+export { setup, createSwap, updateSwapStatus, createAuction };
